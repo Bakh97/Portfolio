a635335c287b9efbb4a4bf50924bde5621a8f8ec
Bakh97 <b.sohi2@gmail.com>
2025-02-28 20:02:05 -0500

    chore: add gh-pages dependency and clean up vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio", // Ensure this matches your repository name
+  base: "/Portfolio", 
 })
